cc80b832802899c5f3d2773af63ac611604a588d
Mohammad Rahmani <rto1680@gmail.com>
2021-02-20 23:02:42 +0430

    Add coloring system

---
+++ lib/themes/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color textColor = const Color(0xff9C9C9D);
+  static Color textColorDark = const Color(0xffffffff);
+
+  static Color bodyColor = const Color(0xffffffff);
+  static Color bodyColorDark = const Color(0xff0E0E0F);
+
+  static Color buttonBackgroundColor = const Color(0xffF7F7F7);
+  static Color buttonBackgroundColorDark = const Color(0xff121212);
+}
